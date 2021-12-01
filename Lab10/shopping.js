3e8be605e47b286ab294ce5ec99471a56fa9db91
Ross Fitzgerald <rossfitzgerald@MacBook-Air.local>
2021-12-01 14:36:38 +0000

    Lab10

---
+++ Lab10/shopping.js
@@ -0,0 +1,32 @@
+var items=[];
+var list=document.getElementById("list");
+
+function listItems(){
+    shoppingList=[];
+    items.forEach(function(element){
+        shoppingList.push("<li>"+element+"</li>");
+    });
+
+    list.innerHTML=shoppingList.join('');
+}
+
+//buttons
+function addToEnd(){
+    items.push(prompt("Enter the item you need to add to the end"));
+    listItems();
+}
+
+function removeFromEnd(){
+    items.pop();
+    listItems();
+}
+
+function addToStart(){
+    items.unshift(prompt("Enter item you want to the list at the top"));
+    listItems();
+}
+
+function removeFromStart(){
+    items.shift();
+    listItems();
+}
